459e58571ee37dba49aeb445782816e5ceb11c23
Jeff Santucci <jeffrey.santucci@gmail.com>
2023-02-13 21:46:02 -0500

    Circles

---
+++ CircleObjects/Circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObjects
+{
+    public class Circle
+    {
+        
+        private double radius;
+
+        // Property to access private radius
+        public double Radius
+        { get { return radius; } set { radius = value; } }
+
+        // Method to calculate the circumference of a circle
+        public double CalculateCircumference()
+        {
+            return 2 * Math.PI * Radius;
+         
+        }
+
+        // Method to calculate the area of a circle
+        public double CalculateArea()
+        {
+            double area = Math.PI * Math.Pow(Radius, 2);
+            return area;
+        }
+        // Constructor
+        public Circle(double radius)
+        {
+            Radius = radius;
+           
+        }
+    }
+
+}
---
+++ CircleObjects/Program.cs
@@ -0,0 +1,42 @@
+﻿// Circle Objects
+// Jeff Santucci
+// 02/09/2023
+
+using CircleObjects;
+
+bool isValidDecimal;
+bool tryAgain;
+double circum;
+int counter = 0;
+
+
+do
+{
+    Console.WriteLine("Please enter the radius for the circle dimensions you wish to see: ");
+    isValidDecimal = double.TryParse(Console.ReadLine(), out double radius);
+
+    if (!isValidDecimal)
+    {
+        Console.WriteLine("You have entered and invalid decimal number");
+        tryAgain = true;
+    }
+    else
+    {
+        Circle circle = new Circle(radius);  // Instatiating the class
+        Console.WriteLine($"Circumference is: {circle.CalculateCircumference()}");
+        Console.WriteLine($"Area is: {circle.CalculateArea()}");
+        counter = counter + 1;
+    }
+    Console.WriteLine("Continue? (y/n)");
+    string cont = Console.ReadLine();
+
+    if (cont == "y")
+    {
+        tryAgain = true;
+    }
+    else
+    {
+        Console.WriteLine("Goodbye. You created " + counter + " Circle object(s).");
+        tryAgain = false;
+    }
+} while (tryAgain);
